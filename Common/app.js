701c5d2a1783b6c399351cf1e7c67f17ba64a669
Kevin Cheng <kcheng094@gmail.com>
2021-03-14 21:17:39 -0700

    create pages

--- Common/app.js
+++ Common/app.js
@@ -1,13 +1,16 @@
 var main = function () {
 
-  jQuery("document").ready(function($){
-    for (i=0;i < 4;i++){
-      $('#outerdiv').append('<div id="innerdiv" class="header">Hi there!</div>');
-      $('#innerdiv').attr('id','X'+i);
+	jQuery("document").ready(function($){
+		for (i=0;i < 4;i++){
+			$('#outerdiv').append('<div id="innerdiv" class="header">Hi there!</div>');
+			$('#innerdiv').attr('id','X'+i);
 
-    };
+		};
 
-  });
+		var today = new Date().toISOString().split('T')[0];
+		document.getElementsByName("start")[0].setAttribute('min', today);
+		document.getElementsByName("end")[0].setAttribute('min', today);
+	});
 
 };
 $(document).ready(main);
