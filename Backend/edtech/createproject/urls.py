4841c6db8f9fb1286680e0a29aa46e2f045d99f1
Kevin Cheng <kcheng094@gmail.com>
2021-03-28 20:57:05 -0700

    display project list in forum

--- Backend/edtech/createproject/urls.py
+++ Backend/edtech/createproject/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'createproject'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('results/', views.results, name='results'),
     path('create_new_project/', views.create_new_project, name='create_new_project'),
 ]
--- Backend/edtech/createproject/views.py
+++ Backend/edtech/createproject/views.py
@@ -3,12 +3,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Projects, SubjectList, StudentLevel, ProjectForm
 from .forms import CreateProjectForm
 from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
     project_list = Projects.objects.order_by('-Start_date')
     context = {'project_list': project_list}
     return render(request, 'createproject/index.html', context)
+
+def results(request):
+    project_list = Projects.objects.order_by('-Start_date')
+    context = {'project_list': project_list}
+    return render(request, 'createproject/results.html', context)
+
+# class DetailView(generic.DetailView):
+#     model = Projects
+#     template_name = 'createproject/detail.html'
+    
+#     def get_queryset(self):
+#         project_list = Projects.objects.order_by('-Start_date')
+#         context = {'project_list': project_list}
+#         return Projects.objects.filter(pub_date__lte=timezone.now())
+
+# class ResultsView(generic.DetailView):
+#     model = Projects
+#     template_name = 'createproject/results.html'
 
 
 def create_new_project(request):
@@ -21,4 +41,4 @@ def create_new_project(request):
         form.save()
 
     context['form'] = form
-    return render(request, 'createproject/index.html', context)
+    return HttpResponseRedirect(reverse('createproject:results'))
