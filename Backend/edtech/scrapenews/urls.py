b4f0667178d2ee0becf6d091ebf5c692eb2c53a6
Kevin Cheng <kcheng094@gmail.com>
2021-04-17 22:37:54 -0400

    cognitive model

--- Backend/edtech/scrapenews/urls.py
+++ Backend/edtech/scrapenews/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = 'scrapenews'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home', views.index, name='index'),
+    path('home2', views.index2, name='index2'),
     path('log_link/', views.log_link, name='log_link'),
+    path('cognitivehome/', views.cognitivehome, name='cognitivehome'),
+    path('cognitive_questionnaire/', views.cognitive_questionnaire, name='cognitive_questionnaire'),
 ]
--- Backend/edtech/scrapenews/views.py
+++ Backend/edtech/scrapenews/views.py
@@ -15,6 +15,15 @@ def index(request):
     context = {'latest_news_list': latest_news_list, 'latest_posts': latest_posts}
     return render(request, 'scrapenews/index.html', context)
 
+def index2(request):
+    for Link in NewsList.objects.values_list('Link', flat=True).distinct():
+        NewsList.objects.filter(pk__in=NewsList.objects.filter(Link=Link).values_list('id', flat=True)[1:]).delete()
+    latest_news_list = NewsList.objects.order_by('-Date')[0:30]
+    latest_posts = PostList.objects.order_by('-PostDate')[0:30]
+    context = {'latest_news_list': latest_news_list, 'latest_posts': latest_posts}
+    
+    return render(request, 'scrapenews/index2.html', context)
+
 def log_link(request):
     context = {}
     form = ArticlesClickedForm(request.POST or None)
@@ -25,3 +34,24 @@ def log_link(request):
 
     context['form'] = form
     return HttpResponseRedirect(reverse('scrapenews:index'))
+
+def cognitive_questionnaire(request):
+    return render(request, 'scrapenews/cognitive.html')
+
+def cognitivehome(request):
+    answers = request.GET
+    fi = 0
+    fd = 0
+    print(answers)
+    for key in answers:
+        if answers[key] == "fi":
+            fi += 1
+        else:
+            fd += 1
+    
+    if fi > fd:
+        print('going to fi page', fd, fi)
+        return HttpResponseRedirect(reverse('scrapenews:index'))
+    else:
+        print('going to fd page', fd, fi)
+        return HttpResponseRedirect(reverse('scrapenews:index2'))
